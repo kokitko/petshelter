828189ea5144828c5d249ca8be063877564c3ca6
Vladyslav Kokitko <koks.vladislav277@gmail.com>
2026-05-04 15:53:39 +0200

    Moving projects to the src directory

---
+++ src/PetShelter.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,84 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using PetShelter.Infrastructure.Persistence;
+using PetShelter.Application.Common.Interfaces.Persistence;
+using PetShelter.Infrastructure.Persistence.Repositories;
+using PetShelter.Application.Common.Interfaces.Authentication;
+using PetShelter.Infrastructure.Authentication;
+using PetShelter.Application.Common.Interfaces.Services;
+using PetShelter.Infrastructure.Services;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace PetShelter.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services, 
+        IConfiguration configuration)
+    {
+        services
+            .AddPersistence(configuration)
+            .AddAuth(configuration);
+
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddPersistence(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services
+            .AddDbContext<PetShelterDbContext>(options => 
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped<IPetRepository, PetRepository>();
+        services.AddScoped<IAppUserRepository, AppUserRepository>();
+        services.AddScoped<IOrgProfileRepository, OrgProfileRepository>();
+        services.AddScoped<IUserProfileRepository, UserProfileRepository>();
+        services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+        services.AddScoped<IPetImageRepository, PetImageRepository>();
+        services.AddScoped<IAdoptionApplicationRepository, AdoptionApplicationRepository>();
+        services.AddScoped<IFileStorageService, FileStorageService>();
+        services.AddScoped<IPasswordHasher, PasswordHasher>();
+        
+        return services;
+    }
+
+    public static IServiceCollection AddAuth(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var jwtSettings = new JwtSettings();
+        configuration.Bind(JwtSettings.SectionName, jwtSettings);
+
+        services.AddSingleton(Options.Create(jwtSettings));
+        services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+
+        services.AddHttpContextAccessor();
+        services.AddScoped<ICurrentUserProvider, CurrentUserProvider>();
+
+        services.AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtSettings.Issuer,
+                ValidAudience = jwtSettings.Audience,
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(jwtSettings.SecretKey))
+            });
+
+        services.AddSingleton<IRefreshTokenGenerator, RefreshTokenGenerator>();
+
+        return services;
+    }
+}
